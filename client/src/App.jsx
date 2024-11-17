2c25304ebda6acdce32f465c842d61b50ccbd96a
SRI-HARIPRIYAN <sris.haripriyan@gmail.com>
2024-11-17 20:05:28 +0530

    update app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,11 @@ import "./App.css";
 import React from "react";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<h1>Hey It's Shree How are you?</h1>
+		</div>
+	);
 };
 
 export default App;
