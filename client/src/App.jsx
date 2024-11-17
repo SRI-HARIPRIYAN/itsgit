83c854b382a37c2a124d2075a3e77c86655c3f86
SRI-HARIPRIYAN <sris.haripriyan@gmail.com>
2024-11-17 20:04:40 +0530

    update app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,11 @@ import React from "react";
 const App = () => {
 	return (
 		<div>
+<<<<<<< HEAD
 			<h1>Hey It's Shree How are you?</h1>
+=======
+			<h1>Hey How are you Its shree</h1>
+>>>>>>> ce66c34 (update app)
 		</div>
 	);
 };
