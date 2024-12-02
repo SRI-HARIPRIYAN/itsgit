db94728f08c65ac3c91524a15c844eef23c7728e
Sri Haripriyan <115647705+SRI-HARIPRIYAN@users.noreply.github.com>
2024-12-02 19:44:19 +0530

    Frontend (#1)
    * update app
    
    * update after rebasing
    
    * update css
    
    * update css on frontend after rebase
    
    * update css on frontend
    
    * deleted readme
    
    * deleted readme
    
    * added Footer to check -a -m
    
    * did nothing but checking the amend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import React from "react";
 const App = () => {
 	return (
 		<div>
-			<h1>Hey It's Shree How are you?</h1>
+			<h1>Hey How are you Its shree</h1>
+			<p>Added feature in main branch to make conflict</p>
 		</div>
 	);
 };
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div>
+			<p>Hello Header added</p>
+			<p>checking amend and commit in a line</p>
+		</div>
+	);
+};
+
+export default Header;
