d249982f660b764dc664dee4ec8677ae7a4f51b3
sri-haripriyan <sris.haripriyan@gmail.com>
2024-12-18 18:46:40 +0530

    updated footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-	return <div>Footer</div>;
+  return (
+    <div>
+      <p>&copy;2025 Sri corp.</p>
+    </div>
+  );
 };
 
 export default Footer;
